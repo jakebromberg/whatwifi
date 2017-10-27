21b5fc4f23b5cd5e9327b971d67d67906379e3a6
Jake Bromberg <jakebromberg@nestlabs.com>
2017-10-27 07:46:50 -0700

    Initial Commit

---
+++ whatwifi/AppDelegate.swift
@@ -0,0 +1,44 @@
+//
+//  AppDelegate.swift
+//  wifiname
+//
+//  Created by Jake Bromberg on 10/24/17.
+//  Copyright © 2017 Flat Cap. All rights reserved.
+//
+
+import Cocoa
+import CoreWLAN
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    let statusItem = NSStatusBar.system.statusItem(withLength:NSStatusItem.squareLength)
+    let wifiClient = CWWiFiClient.shared()
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        statusItem.menu = NSMenu()
+        statusItem.length = NSStatusItem.variableLength
+        
+        updateTitle()
+        
+        wifiClient.delegate = self
+        try! wifiClient.startMonitoringEvent(with: .ssidDidChange)
+    }
+    
+    func updateTitle() {
+        guard let name = CWWiFiClient.shared().interface()?.ssid() else {
+            return
+        }
+        
+        DispatchQueue.main.async {
+            self.statusItem.button?.title = name
+            self.statusItem.button?.setNeedsDisplay()
+        }
+    }
+}
+
+extension AppDelegate: CWEventDelegate {
+    func ssidDidChangeForWiFiInterface(withName interfaceName: String) {
+        updateTitle()
+    }
+}
